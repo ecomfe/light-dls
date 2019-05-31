fc173d33a0dc49d85a945aec060ad3ab4d7e446a
Justineo <justice360@gmail.com>
2019-05-31 23:46:44 +0800

    feat: add naive info color support, adjust aside shadow to docked shadow

--- src/contextual.js
+++ src/contextual.js
@@ -4,6 +4,7 @@ import { isInRanges } from './utils'
 const COLD_RANGES = [[64, 320]]
 
 const CONTEXTUAL_RANGES = {
+  info: [[215, 215]],
   success: [[95, 125]],
   warning: [[18, 24]],
   error: [[0, 5], [355, 360]]
@@ -11,11 +12,13 @@ const CONTEXTUAL_RANGES = {
 
 const CONTEXTUAL_COLORS = {
   cold: {
+    info: [215, 75, 95],
     success: [125, 70, 75],
     warning: [18, 70, 95],
     error: [355, 70, 90]
   },
   warm: {
+    info: [215, 75, 95],
     success: [95, 70, 75],
     warning: [24, 75, 95],
     error: [5, 70, 90]
@@ -45,11 +48,13 @@ export default function shade (less, pluginManager, functions) {
   functions.add('dls-contextual', (base = {}, type = {}) => {
     if (
       !type.value ||
-      ['success', 'warning', 'error'].indexOf(
+      ['info', 'success', 'warning', 'error'].indexOf(
         type.value.trim().toLowerCase()
       ) === -1
     ) {
-      throw new Error('`type` must be one of `success`, `warning` and `error`.')
+      throw new Error(
+        '`type` must be one of `info`, `success`, `warning` and `error`.'
+      )
     }
 
     const color = getContextual(
