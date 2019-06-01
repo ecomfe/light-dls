2e509e56fcec4b8eb81b4ecfa53727ed4ae9251f
Justineo <justice360@gmail.com>
2019-06-02 01:21:32 +0800

    refactor: adjust file structure

--- rollup.config.js
+++ rollup.config.js
@@ -2,7 +2,7 @@ import commonjs from 'rollup-plugin-commonjs'
 import nodeResolve from 'rollup-plugin-node-resolve'
 
 export default {
-  input: 'src/index.js',
+  input: 'src/plugin/index.js',
   output: {
     file: 'dist/index.js',
     format: 'cjs'
