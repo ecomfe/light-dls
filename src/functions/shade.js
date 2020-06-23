478ed17eaacf7f72fb2a7a12087c68ce835acb4d
Justineo <justice360@gmail.com>
2020-06-23 14:02:57 +0800

    feat: use new color palette algorithm, refine tests

--- src/functions/shade.js
+++ src/functions/shade.js
@@ -1,67 +1,115 @@
 import kolor from 'kolor'
 import { isNumber } from '../utils'
 
-const S_MAX = 100
-const S_MIN = 5
-const S_INC_STEP = 5
-
+const BASE_LEVEL = 7
+const DARK_B_MAXES = [85, 65, 40]
+const LIGHT_B_STEP = 20
 const B_MAX = 100
 const B_MIN = 0
-const B_INC_STEP = 5
-const B_DEC_STEP = 15
+const S_MAX = 100
+const S_MIN = 5
+const B_CLAMP_S_DEVIATION_RATIO = 0.2
+const LOW_S_DEVIATION_RATIO = 0.4
+const S_COEF = 3.2
 
-const H_STEP = 1
-
-function wrap (value, min, max) {
+function clamp (value, min, max) {
   if (min > max) {
-    max = min + max
-    min = max - min
-    max = max - min
+    [min, max] = [max, min]
   }
-  const period = max - min
-  return min + (((value % period) + period) % period)
+  return Math.min(max, Math.max(min, value))
 }
 
-function deg (value) {
-  return wrap(value, 0, 360)
+function getBrightness (base, level) {
+  if (level === BASE_LEVEL) {
+    return base
+  }
+
+  // dark shades
+  if (level > BASE_LEVEL) {
+    if (base <= B_MIN) {
+      return base
+    }
+
+    return (
+      B_MIN +
+      ((DARK_B_MAXES[level - BASE_LEVEL - 1] - B_MIN) * (base - B_MIN)) /
+        (B_MAX - B_MIN)
+    )
+  }
+
+  // light shades
+  return base + (BASE_LEVEL - level) * LIGHT_B_STEP
+}
+
+function getSatuation (base, level) {
+  if (level === BASE_LEVEL) {
+    return base
+  }
+
+  // light shades
+  if (level > BASE_LEVEL) {
+    level--
+  }
+
+  let pl = BASE_LEVEL - 1
+  const portion =
+    (S_COEF * level * level +
+      ((100 - (pl * pl - 1) * S_COEF) * level) / (pl - 1) +
+      ((pl * pl - 1) * S_COEF - 100) / (pl - 1) -
+      S_COEF) /
+    100
+
+  if (base > S_MIN) {
+    return (base - S_MIN) * portion + S_MIN
+  }
+
+  return portion * base
 }
 
 function getShade ([h, rawS, rawB], level) {
   const s = rawS * 100
   const b = rawB * 100
-  const sDecStep = Math.round(s / 5)
 
-  if (level <= 5) {
-    let i = level - 1
-    return [
-      deg(Math.round(h + (5 - i) * H_STEP)),
-      Math.round(Math.max(S_MIN, s - (5 - i) * sDecStep)) / 100,
-      Math.round(Math.min(B_MAX, b + (5 - i) * B_INC_STEP)) / 100
-    ]
-  } else if (level === 6) {
-    return [Math.round(h), Math.round(s) / 100, Math.round(b) / 100]
-  } else {
-    let i = level - 6
-    return [
-      deg(Math.round(h - i * H_STEP)),
-      Math.round(Math.min(S_MAX, s + i * S_INC_STEP)) / 100,
-      Math.round(Math.max(B_MIN, b - i * B_DEC_STEP)) / 100
-    ]
+  if (level === BASE_LEVEL) {
+    return [h, s, b]
   }
+
+  let brightness = getBrightness(b, level)
+  let deviation =
+    s > 0 ? ((Math.log2(100 / s) * s) / 100) * s * LOW_S_DEVIATION_RATIO : 0
+
+  if (level < BASE_LEVEL) {
+    let bPrev = getBrightness(b, BASE_LEVEL - 1)
+    // brightness may be clamped
+    deviation +=
+      bPrev > B_MAX
+        ? ((bPrev - B_MAX) / LIGHT_B_STEP) * B_CLAMP_S_DEVIATION_RATIO * s
+        : 0
+  }
+
+  let saturation = getSatuation(s - deviation, level)
+
+  return [h, clamp(saturation, 0, S_MAX), clamp(brightness, B_MIN, B_MAX)]
 }
 
-export default function install (less, pluginManager, functions) {
+function normalize ([h, s, b]) {
+  return [Math.round(h), Math.round(s) / 100, Math.round(b) / 100]
+}
+
+export default function install (less, _, functions) {
   functions.add('dls-shade', (base = {}, level = {}) => {
     if (!isNumber(level.value) || level.value < 1 || level.value > 10) {
       throw new Error('`level` should be a number that ≥ 1 and ≤ 10.')
     }
 
-    const color = getShade(
-      kolor
-        .rgb(base.rgb)
-        .hsv()
-        .toArray(),
-      level.value
+    const color = normalize(
+      getShade(
+        kolor
+          .rgb(base.rgb)
+          .hsv()
+          .toArray(),
+        level.value
+      )
     )
 
     return less.color(
