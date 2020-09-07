bc9f895fb63d5d4702a1dac54e455182087dac38
mahaina <nana1346@sina.com>
2020-09-07 16:48:12 +0800

    refactor: refector to monorepo

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,5 +14,14 @@ module.exports = {
     'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
     'no-multi-spaces': ['error', { ignoreEOLComments: true }],
     'standard/no-callback-literal': 0
-  }
+  },
+  'overrides': [
+    {
+      files: ['packages/dls-color-palette/**/*.spec.js'],
+      env: {
+        es6: true,
+        jest: true
+      }
+    }
+  ]
 }
