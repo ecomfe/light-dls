6ca35e896b2fbcc921b6a24c4568b2f2bff250c0
Justineo <justice360@gmail.com>
2021-08-30 11:33:55 +0800

    refactor: update to postcss 8 and refactor rewrite-keyframes

--- packages/dls-graphics/build/css.js
+++ packages/dls-graphics/build/css.js
@@ -1,5 +1,6 @@
 import postcss from 'postcss'
 import cssnano from 'cssnano'
+import rewriteKeyframes from './rewrite-keyframes'
 
 export async function process (cssText, id) {
   return postcss([
@@ -11,55 +12,3 @@ export async function process (cssText, id) {
       console.error(error)
     })
 }
-
-/**
- * Rewrite keyframes according to unique ids
- * Code mostly come from @vue/component-compiler-utils
- * See: https://github.com/vuejs/component-compiler-utils/blob/8b2c64618c47ea2dd3b38691286f825cbbcf9468/lib/stylePlugins/scoped.ts
- */
-const keyframesStore = {}
-
-const rewriteKeyframes = postcss.plugin('rewrite-keyframes', ({ id }) => {
-  return (root) => {
-    root.each((node) => {
-      if (node.type === 'atrule') {
-        if (/-?keyframes$/.test(node.name)) {
-          if (!keyframesStore[id]) {
-            keyframesStore[id] = {}
-          }
-          keyframesStore[id][node.params] = node.params = `${node.params}-${id}`
-        }
-      }
-    })
-
-    const keyframes = keyframesStore[id] || {}
-
-    if (Object.keys(keyframes || {}).length) {
-      root.walkDecls((decl) => {
-        // individual animation-name declaration
-        if (/^(-\w+-)?animation-name$/.test(decl.prop)) {
-          decl.value = decl.value
-            .split(',')
-            .map((v) => keyframes[v.trim()] || v.trim())
-            .join(',')
-        }
-        // shorthand
-        if (/^(-\w+-)?animation$/.test(decl.prop)) {
-          decl.value = decl.value
-            .split(',')
-            .map((v) => {
-              const vals = v.trim().split(/\s+/)
-              const i = vals.findIndex((val) => keyframes[val])
-              if (i !== -1) {
-                vals.splice(i, 1, keyframes[vals[i]])
-                return vals.join(' ')
-              } else {
-                return v
-              }
-            })
-            .join(',')
-        }
-      })
-    }
-  }
-})
---
+++ packages/dls-graphics/build/rewrite-keyframes.js
@@ -0,0 +1,58 @@
+/**
+ * Rewrite keyframes according to unique ids
+ * Code mostly come from @vue/component-compiler-utils
+ * See: https://github.com/vuejs/component-compiler-utils/blob/8b2c64618c47ea2dd3b38691286f825cbbcf9468/lib/stylePlugins/scoped.ts
+ */
+const keyframesStore = {}
+
+function rewriteKeyframes ({ id }) {
+  return {
+    postcssPlugin: 'rewrite-keyframes',
+    Once (root) {
+      root.each((node) => {
+        if (node.type === 'atrule') {
+          if (/-?keyframes$/.test(node.name)) {
+            if (!keyframesStore[id]) {
+              keyframesStore[id] = {}
+            }
+            keyframesStore[id][node.params] = node.params = `${node.params}-${id}`
+          }
+        }
+      })
+
+      const keyframes = keyframesStore[id] || {}
+
+      if (Object.keys(keyframes || {}).length) {
+        root.walkDecls((decl) => {
+        // individual animation-name declaration
+          if (/^(-\w+-)?animation-name$/.test(decl.prop)) {
+            decl.value = decl.value
+              .split(',')
+              .map((v) => keyframes[v.trim()] || v.trim())
+              .join(',')
+          }
+          // shorthand
+          if (/^(-\w+-)?animation$/.test(decl.prop)) {
+            decl.value = decl.value
+              .split(',')
+              .map((v) => {
+                const vals = v.trim().split(/\s+/)
+                const i = vals.findIndex((val) => keyframes[val])
+                if (i !== -1) {
+                  vals.splice(i, 1, keyframes[vals[i]])
+                  return vals.join(' ')
+                } else {
+                  return v
+                }
+              })
+              .join(',')
+          }
+        })
+      }
+    }
+  }
+}
+
+rewriteKeyframes.postcss = true
+
+export default rewriteKeyframes
